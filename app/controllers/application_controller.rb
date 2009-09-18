81edfac3b154080d80dd4ab34634a54fecb5858f
Steven Wilkin <stevenwilkin@gmail.com>
2009-09-18 17:50:33 +0100

    account info box with login/logout links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+	helper_method :current_user
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+	def current_user
+		session[:user_name]
+	end
+
 end
